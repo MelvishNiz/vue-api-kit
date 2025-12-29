f7319750a8a2176b0d6ec5dacb9a2c036cd3daf8
Muhammad Syahrul <msyahrul801@gmail.com>
2025-12-29 16:37:10 +0700

    fix: change ZodError export from type to named export in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,5 +2,5 @@ export * as z from "zod";
 export { createApiClient } from "./core/client";
 export { mergeQueries, mergeMutations, defineQuery, defineMutation } from "./core/merge";
 export type * from "./core/types";
-export type { ZodError } from "zod";
+export { ZodError } from "zod";
 export { AxiosError } from "axios";
